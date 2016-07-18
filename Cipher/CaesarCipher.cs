16d6cd375f817fddf1b7287fda77e688742100ea
Dom Edwards <dom97@vt.edu>
2016-07-18 13:34:50 -0400

    inital commit

---
+++ Cipher/CaesarCipher.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cipher
+{
+    public class CaesarCipher
+    {
+        private Int32 key;
+        private static Int32 firstAscii = 65;
+        private static Int32 alphabetLength = 26;
+
+        public CaesarCipher(Int32 key)
+        {
+            this.key = key;
+        }
+
+        public String Encode(String text)
+        {
+            return CodeText(text, true);
+        }
+
+        public String Decode(String cipherText)
+        {
+            return CodeText(cipherText, false);
+        }
+
+        private Char EncodeCharacter(Char character)
+        {
+            var ascii = (Int32)character;
+            ascii = (ascii + key - firstAscii) % alphabetLength + firstAscii;
+            return (Char)ascii;
+        }
+
+        private Char DecodeCharacter(Char character)
+        {
+            var ascii = (Int32)character;
+            var letter = ascii - firstAscii;
+            letter = letter - key;
+
+            if (letter < 0)
+                letter = alphabetLength + letter;
+
+            return (Char)(letter + firstAscii); 
+        }
+
+        private String CodeText(String text, Boolean encode)
+        {
+            var builder = new StringBuilder();
+            text = text.ToUpper();
+
+            for (var i = 0; i < text.Length; i++)
+            {
+                var currentCharacter = text.ElementAt(i);
+
+                if (Char.IsLetter(currentCharacter))
+                {
+                    if (encode)
+                    {
+                        var enocdedChar = EncodeCharacter(currentCharacter);
+                        builder.Append(enocdedChar);
+                    }
+                    else
+                    {
+                        var decodedChar = DecodeCharacter(currentCharacter);
+                        builder.Append(decodedChar);
+                    }
+                }
+                else
+                {
+                    builder.Append(currentCharacter);
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ CipherTests/CipherTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cipher;
+
+namespace CipherTests
+{
+    [TestClass]
+    public class CipherTests
+    {
+        private CaesarCipher caesar;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            caesar = new CaesarCipher(3);
+        }
+
+        [TestMethod]
+        public void SingleCharacterIsEncodedCorrectly()
+        {
+            Assert.AreEqual("D", caesar.Encode("A"));
+            Assert.AreEqual("A", caesar.Encode("X"));
+        }
+
+        [TestMethod]
+        public void SingleWordIsEncodedCorrectly()
+        {
+            Assert.AreEqual("FRGH", caesar.Encode("Code"));
+        }
+
+        [TestMethod]
+        public void PhraseIsEncdodedCorrectly()
+        {
+            Assert.AreEqual("ZKDW LV XS GDZJC", caesar.Encode("What is up dawgz"));
+        }
+
+        [TestMethod]
+        public void SingleCharacterIsDecodedCorrectly()
+        {
+            Assert.AreEqual("X", caesar.Decode("A"));
+            Assert.AreEqual("A", caesar.Decode("D"));
+        }
+
+        [TestMethod]
+        public void SingleWordIsDecodedCorrectly()
+        {
+            Assert.AreEqual("CODE", caesar.Decode("FRGH"));
+        }
+
+        [TestMethod]
+        public void PhraseIsDecdodedCorrectly()
+        {
+            Assert.AreEqual("WHAT IS UP DAWGZ", caesar.Decode("ZKDW LV XS GDZJC"));
+        }
+    }
+}
